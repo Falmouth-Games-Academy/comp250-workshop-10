35896e596c8c1bf7f3be2d6ba724cf33bd3038d5
Ed Powley <edpowley@gmail.com>
2019-04-02 16:05:59 +0100

    Evolutionary algorithm

--- main.py
+++ main.py
@@ -35,6 +35,40 @@ def calculate_fitness(the_map):
     return pathfinding.get_path_length(path)
 
 
+def get_fittest(population):
+    fittest_map = None
+    fittest_fitness = 0
+
+    for tiles in population:
+        the_map = map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
+        fitness = calculate_fitness(the_map)
+        if fittest_map is None or fitness > fittest_fitness:
+            fittest_map = tiles
+            fittest_fitness = fitness
+
+    return (fittest_map, fittest_fitness)
+
+
+def tournament_select(population):
+    TOURNAMENT_SIZE = 10
+    tournament = [random.choice(population) for i in range(TOURNAMENT_SIZE)]
+    (fittest_map, fittest_fitness) = get_fittest(tournament)
+    return fittest_map
+
+
+def mutate(tiles):
+    tiles = tiles[:]
+
+    for i in range(10):
+        index = random.randrange(len(tiles))
+        if tiles[index] == ' ':
+            tiles[index] = '*'
+        elif tiles[index] == '*':
+            tiles[index] = ' '
+
+    return tiles
+
+
 def main():
     # Initialise PyGame
     pygame.init()
@@ -52,29 +86,41 @@ def main():
     tiles[0] = 'S'
     tiles[-1] = 'G'
 
-    the_map = map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
-    fitness = calculate_fitness(the_map)
+    POPULATION_SIZE = 20
+
+    population = [tiles[:] for n in range(POPULATION_SIZE)]
+
+    # map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)
 
     while True:
-        new_tiles = tiles[:]
-        tile_index = random.randrange(len(new_tiles))
-        if new_tiles[tile_index] == ' ':
-            new_tiles[tile_index] = '*'
-        elif new_tiles[tile_index] == '*':
-            new_tiles[tile_index] = ' '
-        new_map = map.Map((MAP_WIDTH, MAP_HEIGHT), new_tiles, TILE_SIZE, include_diagonals=True)
-        new_fitness = calculate_fitness(new_map)
+        new_population = []
 
-        if new_fitness > fitness:
-            tiles = new_tiles
-            the_map = new_map
-            fitness = new_fitness
+        population_fitness = [
+            (calculate_fitness(map.Map((MAP_WIDTH, MAP_HEIGHT), tiles, TILE_SIZE, include_diagonals=True)), tiles)
+            for tiles in population
+            ]
+        population_fitness.sort()
 
-            print("Current fitness:", fitness)
-            path = get_path(the_map)
-            draw_map_and_path(screen, the_map, path)
+        elite = population_fitness[-3:]
+        print(elite)
+        for (fitness, tiles) in elite:
+            new_population.append(tiles)
 
-            pygame.event.get()
+        while len(new_population) < POPULATION_SIZE:
+            parent = tournament_select(population)
+            child = mutate(parent)
+            new_population.append(child)
+
+        (fittest_tiles, fittest_fitness) = get_fittest(new_population)
+        fittest_map = map.Map((MAP_WIDTH, MAP_HEIGHT), fittest_tiles, TILE_SIZE, include_diagonals=True)
+
+        print("Current fitness:", fittest_fitness)
+        path = get_path(fittest_map)
+        draw_map_and_path(screen, fittest_map, path)
+
+        pygame.event.get()
+
+        population = new_population
 
 if __name__ == '__main__':
     main()
